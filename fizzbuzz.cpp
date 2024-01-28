2caaaf21540bdc071f728e99e68a99e4448cf8ce
Kian <kianp20@vt.edu>
2024-01-27 19:41:45 -0500

    Added fizzbuzz and languages.txt

---
+++ fizzbuzz.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+
+int main()
+{
+    for (int i = 1; i <= 20; i++){
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            std::cout << " Fizz Buzz,";
+        }
+        else if (i % 3 == 0) {
+            std::cout << " Fizz,";
+        }
+        else if (i % 5 == 0) {
+            std::cout << " Buzz,";
+        }
+        else {
+            std::cout << " " << i << ",";
+        }
+    }
+}
